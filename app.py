cc94c88a3d602f919e9abc09dd962d8526f14e86
Bang Cogan <awas.enak123@gmail.com>
2025-10-24 02:48:33 +0700

    Use env APP_MESSAGE and PORT; run on 0.0.0.0:8080

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from DevOps Simulation!"
+    msg = os.getenv("APP_MESSAGE", "Hello from DevOps Simulation!")
+    return msg
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
