a643101e97a882df56c8578b96b3591459c36a4b
Bang Cogan <awas.enak123@gmail.com>
2025-10-25 03:45:40 +0700

    Add fake login page

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     msg = os.getenv("APP_MESSAGE", "Hello from CNAPP World!")
     return render_template('index.html', message=msg)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Demo login logic (tidak sungguhan)
+        if username == 'admin' and password == '1234':
+            return render_template('index.html', message=f"Welcome, {username}!")
+        else:
+            return render_template('login.html', error="Invalid credentials. Try again.")
+
+    return render_template('login.html')
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
