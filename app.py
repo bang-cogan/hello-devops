6112f2948705f9b8ba8eb8237f8665f498982146
Bang Cogan <awas.enak123@gmail.com>
2025-10-25 01:52:49 +0700

    Add HTML template with Bootstrap styling

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    msg = os.getenv("APP_MESSAGE", "Hello from DevOps Simulation!")
-    return msg
+    msg = os.getenv("APP_MESSAGE", "Hello from CNAPP World!")
+    return render_template('index.html', message=msg)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
